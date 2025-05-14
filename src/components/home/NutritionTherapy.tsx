90d7d86626ea38d3466aad10879d21335778310a
sherifemad10 <sherifemad.web@gmail.com>
2025-05-14 21:20:18 +0300

    hero

--- src/components/home/NutritionTherapy.tsx
+++ src/components/home/NutritionTherapy.tsx
@@ -81,7 +81,7 @@ const NutritionTherapy: React.FC = () => {
           </h2>
           <div className="bg-neutral-100 dark:bg-neutral-800 rounded-2xl overflow-hidden">
             <img
-              src="src/assets/cura.jpg"
+              src="./drsherif.png"
               alt="Nutritional Therapy"
               className="w-full h-80 object-cover"
             />
